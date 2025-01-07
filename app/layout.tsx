21f6348adc081c830feb37a4cb655050d7b04b41
Meng <43998638+Meng-V@users.noreply.github.com>
2025-01-07 16:11:20 -0500

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import { Inter } from "next/font/google";
+import { Header } from "@/components/Header";
+import Footer from "@/components/Footer";
+import "./globals.css";
+
+export const metadata = {
+  metadataBase: new URL("https://daaho.lib.miamioh.edu"),
+  title: "DAAHO - Documenting Asian American Histories in Ohio",
+  description: "The DAAHO project serves as a central hub for exploring the stories, achievements, and enduring contributions of Asian American citizens and diasporic Asians in Ohio.",
+};
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={inter.variable}>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,138 @@
+'use client'
+
+import {
+  Popover,
+  PopoverButton,
+  PopoverBackdrop,
+  PopoverPanel,
+} from '@headlessui/react'
+
+import { Button } from '@/components/Button'
+import { Container } from '@/components/Container'
+import Logo from "@/images/logos/logo.jpg"
+
+import Link from 'next/link'
+import clsx from 'clsx'
+import Image from 'next/image'
+
+
+
+function NavLink({
+  href,
+  children,
+}: {
+  href: string
+  children: React.ReactNode
+}) {
+  return (
+    <Link
+      href={href}
+      className="inline-block rounded-lg px-2 py-1 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900"
+    >
+      {children}
+    </Link>
+  )
+}
+
+
+function MobileNavLink({
+  href,
+  children,
+}: {
+  href: string
+  children: React.ReactNode
+}) {
+  return (
+    <PopoverButton as={Link} href={href} className="block w-full p-2">
+      {children}
+    </PopoverButton>
+  )
+}
+
+function MobileNavIcon({ open }: { open: boolean }) {
+  return (
+    <svg
+      aria-hidden="true"
+      className="h-3.5 w-3.5 overflow-visible stroke-slate-700"
+      fill="none"
+      strokeWidth={2}
+      strokeLinecap="round"
+    >
+      <path
+        d="M0 1H14M0 7H14M0 13H14"
+        className={clsx(
+          'origin-center transition',
+          open && 'scale-90 opacity-0',
+        )}
+      />
+      <path
+        d="M2 2L12 12M12 2L2 12"
+        className={clsx(
+          'origin-center transition',
+          !open && 'scale-90 opacity-0',
+        )}
+      />
+    </svg>
+  )
+}
+
+function MobileNavigation() {
+  return (
+    <Popover>
+      <PopoverButton
+        className="relative z-10 flex h-8 w-8 items-center justify-center ui-not-focus-visible:outline-none"
+        aria-label="Toggle Navigation"
+      >
+        {({ open }) => <MobileNavIcon open={open} />}
+      </PopoverButton>
+      <PopoverBackdrop
+        transition
+        className="fixed inset-0 bg-slate-300/50 duration-150 data-[closed]:opacity-0 data-[enter]:ease-out data-[leave]:ease-in"
+      />
+      <PopoverPanel
+        transition
+        className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5 data-[closed]:scale-95 data-[closed]:opacity-0 data-[enter]:duration-150 data-[leave]:duration-100 data-[enter]:ease-out data-[leave]:ease-in"
+      >
+        <MobileNavLink href="#features">About</MobileNavLink>
+        <MobileNavLink href="#testimonials">Events</MobileNavLink>
+        <MobileNavLink href="#pricing">News</MobileNavLink>
+        <hr className="m-2 border-slate-300/40" />
+        <MobileNavLink href="/login">Get Involved</MobileNavLink>
+      </PopoverPanel>
+    </Popover>
+  )
+}
+
+export function Header() {
+  return (
+    <header className="py-10 bg-orange-50">
+      <Container>
+        <nav className="relative z-50 flex justify-between">
+          <div className="flex items-center md:gap-x-12">
+            <Link href="#" aria-label="Home">
+              <Image className="h-24 w-auto" src={Logo} alt={'DAAHO Logo'}/>
+            </Link>
+            <div className="hidden md:flex md:gap-x-6">
+              <NavLink href="#features">About</NavLink>
+              <NavLink href="#testimonials">Events</NavLink>
+              <NavLink href="#pricing">News</NavLink>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-5 md:gap-x-8">
+            <div className="hidden md:block">
+              <NavLink href="/login">Get Involved</NavLink>
+            </div>
+            <Button href="/register" color="red">
+              <span>
+                Contact us <span className="hidden lg:inline">here</span>
+              </span>
+            </Button>
+            <div className="-mr-1 md:hidden">
+              <MobileNavigation />
+            </div>
+          </div>
+        </nav>
+      </Container>
+    </header>
+  )
+}
